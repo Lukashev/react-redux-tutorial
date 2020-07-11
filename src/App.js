3f22710b74ea2eb9f530f5ff5ad12118240c5440
Lukashev <lnv161297@gmail.com>
2020-07-11 10:04:42 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import logger from "redux-logger"
+import { createStore, applyMiddleware, compose } from "redux"
+import "./App.css"
+import reducers from "./store/reducers"
+import Main from "./components/Main"
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
